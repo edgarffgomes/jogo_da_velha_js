ca759061e221825db1aaec65e8b45f28e7c3ca2c
Edgar Gomes <edgargomes.iff@gmail.com>
2021-10-25 13:28:24 -0300

    Implementação javascript para definição de vitória, derrota ou empate adicionada. Botão para reiniciar a partida adicionado. Projeto finalizado!

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const player1 = 'X'
 const player2 = 'O'
 var playTime = player1
 var gameOver = false
+var movimentos=0;
 
 
 atualizaMostrador()
@@ -44,9 +45,60 @@ function inicializarEspacos(){
 					this.setAttribute("jogada", player2)
 					playTime = player1
 				}
+				movimentos++;
 				atualizaMostrador()
+				verificarVencedor()
 			}
 		});
 				
 	}
 }
+
+async function verificarVencedor(){
+	var a1 = document.getElementById('a1').getAttribute("jogada")
+	var a2 = document.getElementById('a2').getAttribute("jogada")
+	var a3 = document.getElementById('a3').getAttribute("jogada")
+
+	var b1 = document.getElementById('b1').getAttribute("jogada")
+	var b2 = document.getElementById('b2').getAttribute("jogada")
+	var b3 = document.getElementById('b3').getAttribute("jogada")
+
+	var c1 = document.getElementById('c1').getAttribute("jogada")
+	var c2 = document.getElementById('c2').getAttribute("jogada")
+	var c3 = document.getElementById('c3').getAttribute("jogada")
+
+	var vencedor = ""
+
+	if((a1 == b1 && a1 == c1 && a1 != "") || (a1 == b2 && a1 == c3 && a1 != "") || (a1 == a2 && a1 == a3 && a1 != "")){
+		vencedor = a1
+	}
+	if((b2 == a2 && b2 == c2 && b2 != "") || (b2 == a3 && b2 == c1 && b2 != "") || (b2 == b1 && b2 == b3 && b2 != "")){
+		vencedor = b2
+	}
+	if((c3 == a3 && c3 == b3  && c3 != "") || (c3 == c1 && c3 == c2  && c3 != "")){
+		vencedor = c3
+	}
+
+	if(vencedor != "" && vencedor != null){
+		gameOver = true
+
+		await sleep(50)
+		alert('O ganhador foi o: ' + vencedor)
+	}else{
+		if(movimentos == 9){
+			await sleep(50)
+			alert('Deu velha!')
+		}
+	}
+	
+	function sleep(ms){
+
+		return new Promise(resolve => setTimeout(resolve, ms))
+	}
+}
+
+function reiniciarPartida(){
+	window.location.href = "index.html"
+}
+
+
